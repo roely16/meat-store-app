9bcf6897ebc161bfe9384db162046d1a6fc5dff3
Roely Chur <gerson.roely@gmail.com>
2022-01-08 12:47:21 -0600

    first commit

---
+++ src/components/SiteConstruccion.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                   <v-img
+                        :src="require('../assets/logo_store.png')"
+                        class="my-3"
+                        contain
+                        height="400"
+                    />
+                </v-col>
+                <v-col cols="12">
+                    <h1 class="text-center">Próximamente...</h1>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
